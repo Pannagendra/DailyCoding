3822f87a20652612f44f932a927e63b415046a2b
Pannagendra <78739102+Pannagendra@users.noreply.github.com>
2025-08-01 16:53:34 +0530

    Update p19.py

--- p19.py
+++ p19.py
@@ -22,5 +22,5 @@ class solution(object):
       row = [i] *(i+1)
       for j in range(1,i):
         row[j] = triangle[i-1][j-1] + triangle[i-1][j]
-        triangle.append(row)
+      triangle.append(row)
     return triangle
