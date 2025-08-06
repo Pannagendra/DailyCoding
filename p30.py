47ed96637d90d5edb5ee42c79a12fe12476c1bb2
Pannagendra <78739102+Pannagendra@users.noreply.github.com>
2025-08-06 13:30:20 +0530

    Create p30.py

---
+++ p30.py
@@ -0,0 +1,17 @@
+"""
+Given a string s in Roman number format, your task is to convert it to an integer. Various symbols and their values are given below.
+Note: I = 1, V = 5, X = 10, L = 50, C = 100, D = 500, M = 1000
+"""
+class Solution:
+    def romanToDecimal(self, s):
+      roman_values= { 'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+      total =0
+      prev_values =0
+      for char in reversed(s):
+        value = roman_values[char]
+        if val>= prev_value:
+          total+=val
+        else:
+          total-=val
+        prev_val =val
+      return total
