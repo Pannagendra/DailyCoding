67b8aa83117d4d7c3979f146ae22bc075f985159
Pannagendra <78739102+Pannagendra@users.noreply.github.com>
2025-08-12 13:42:45 +0530

    Create p39.py

---
+++ p39.py
@@ -0,0 +1,21 @@
+"""
+Given two positive integers n and x.
+
+Return the number of ways n can be expressed as the sum of the xth power of unique positive integers, in other words, the number of sets of unique integers [n1, n2, ..., nk] where n = n1x + n2x + ... + nkx.
+
+Since the result can be very large, return it modulo 109 + 7.
+
+For example, if n = 160 and x = 3, one way to express n is n = 23 + 33 + 53.
+"""
+class Solution:
+    def numberOfWays(self, n, x):
+        MOD = 10**9 + 7
+        dp = [[0] * (n + 1) for _ in range(n + 1)]
+        dp[0][0] = 1
+        for i in range(1, n + 1):
+            val = i**x
+            for j in range(n + 1):
+                dp[i][j] = dp[i - 1][j]
+                if j >= val:
+                    dp[i][j] = (dp[i][j] + dp[i - 1][j - val]) % MOD
+        return dp[n][n]
