dd69c8927df3e3ca9565ac53cd2b6b99c9432499
Pannagendra <78739102+Pannagendra@users.noreply.github.com>
2025-08-15 17:21:42 +0530

    Create p44.py

---
+++ p44.py
@@ -0,0 +1,12 @@
+"""
+Given an integer n, return true if it is a power of four. Otherwise, return false.
+
+An integer n is a power of four, if there exists an integer x such that n == 4x.
+"""
+class solution(object):
+  def poweroffour(self,n):
+    if n<=0:
+      return False
+    while(n%4 == 0):
+      n//=4
+    return n==1
