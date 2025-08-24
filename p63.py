f3b222eac7e2b2f00e0e3d6877f153fe6851eccc
Pannagendra <78739102+Pannagendra@users.noreply.github.com>
2025-08-24 12:20:47 +0530

    Create p63.py

---
+++ p63.py
@@ -0,0 +1,39 @@
+"""
+Given a binary array nums, you should delete one element from it.
+
+Return the size of the longest non-empty subarray containing only 1's in the resulting array. Return 0 if there is no such subarray.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,0,1]
+Output: 3
+Explanation: After deleting the number in position 2, [1,1,1] contains 3 numbers with value of 1's.
+
+"""
+
+class Solution(object):
+    def longestSubarray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        left = 0
+        zero_count = 0
+        max_len = 0
+        
+        for right in range(len(nums)):
+            if nums[right] == 0:
+                zero_count += 1
+            
+            # Shrink window if more than one zero
+            while zero_count > 1:
+                if nums[left] == 0:
+                    zero_count -= 1
+                left += 1
+            
+            # Window size minus one (because we must delete one element)
+            max_len = max(max_len, right - left)
+        
+        return max_len
