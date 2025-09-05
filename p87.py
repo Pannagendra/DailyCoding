6c3409d638a5a311201b5270a275b5afc68ed184
Pannagendra <78739102+Pannagendra@users.noreply.github.com>
2025-09-05 14:26:52 +0530

    Create p87.py

---
+++ p87.py
@@ -0,0 +1,18 @@
+"""
+You are given two integers num1 and num2.
+
+In one operation, you can choose integer i in the range [0, 60] and subtract 2i + num2 from num1.
+
+Return the integer denoting the minimum number of operations needed to make num1 equal to 0.
+
+If it is impossible to make num1 equal to 0, return -1.
+"""
+class Solution:
+  def makeTheIntegerZero(self, num1: int, num2: int) -> int:
+    k=1
+    x=num1-num2*k
+    if x<k:
+      return -1
+    if k>=x.bit_count():
+      return k
+    k+=1
